306193ef94930182b1e6cbf83b0dd812b2c8affb
CraftyElites <enochatenaga@gmail.com>
2025-12-21 18:11:00 +0100

    Fix sendFile path in index.cjs

--- server/index.cjs
+++ server/index.cjs
@@ -789,7 +789,7 @@ app.get('/api/app-update', async (req, res) => {
 
   app.get('/', async (req, res) => {
     try {
-      res.sendFile(path.join(__dirname, './'));
+      res.sendFile(path.join(__dirname));
     } catch(error) {
 
     }
